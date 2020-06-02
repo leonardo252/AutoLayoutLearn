c3ada867eb9e59c13305d2d8831e082670a3f77c
Leonardo Gomes Fernandes <leonardo-lol252@hotmail.com>
2020-06-02 15:08:26 -0300

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Leonardo Gomes on 02/06/20.
+//  Copyright © 2020 Leonardo Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
